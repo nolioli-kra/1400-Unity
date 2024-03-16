6ae557c3925bec3dbfc98fcf8d51489a235226d7
Nolan <11054347@uvu.edu>
2024-03-16 01:20:08 -0600

    Animal Stampede

---
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float xBound = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //stay in-bounds
+        if(transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+
+        //launch projectile
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
